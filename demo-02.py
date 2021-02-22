4b60752e9d069996457984cf43032044a105b618
TPLxxA <31654113+TPLxxA@users.noreply.github.com>
2021-02-22 11:48:01 +0100

    added demos

---
+++ demo-02.py
@@ -0,0 +1,43 @@
+# Operators
+# addition
+x = 10
+y = 15
+z = x + y
+print(z)
+
+# subtraction
+x = 100
+y = 60
+z = x - y
+print(z)
+
+# multiplication
+x = 5
+y = 5
+z = 5 * 5
+print(z)
+
+# division
+x = 100
+y = 25
+z = x / z
+print(z)
+
+# Bonus
+# powers
+x = 3
+y = 2
+z = x**y
+print(z)
+
+# floor division
+x = 13
+y = 4
+z = x // y
+print(z)
+
+# modulo
+x = 25
+y = 2
+z = x % y
+print(z)
---
+++ demo-03.py
@@ -0,0 +1,10 @@
+# for loops
+# using range
+for i in range(10):
+    print(i)
+
+# Bonus
+# looping over a list
+mylist = ["item 1", "item 2", "item 3"]
+for item in list:
+    print(item)
